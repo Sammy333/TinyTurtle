10b81b0ecaa8ebddd3a1d08e9efbfa6327f045fb
Sam Uwog <suwoghiren@cristoreyny.org>
2017-02-16 14:57:25 +0000

    I started to create my square

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 TinyTurtle.apply(window);
-forward(45);
-right(180);
-forward(90);
+
+function square (width);
+
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(45);
+}
+
+function square (x) {
+    square(2*x);
+    square()
+}
+
+
+
 stamp();
