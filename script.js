d7567edd6a637f534c8e3d2aa5a74a85dee17f8d
Sam Uwog <suwoghiren@cristoreyny.org>
2017-02-17 14:35:23 +0000

    I changed the size and orientation of the squares. I also added more squares.

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 TinyTurtle.apply(window);
 
-function square (width);
+function square (width){
 
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(45);
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(90);
+forward(width);
+right(45);
 forward(width);
 right(90);
 forward(width);
@@ -12,11 +28,13 @@ forward(width);
 right(45);
 }
 
-function square (x) {
-    square(2*x);
-    square()
+function shape(x) {
+square(2*x);
+square(x);
+square(x+15);
+square(x+30);
 }
 
-
-
 stamp();
+
+shape(34);
